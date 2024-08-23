f6c9942c7cbcdfdc8a04f92de8f964aafe845c0d
Jasper Samson <s1136647@student.hsleiden.nl>
2024-08-23 20:36:39 +0200

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        @include('layout.header')
+        @yield('content')
+        @include('layout.footer')
+    </body>
+</html>
